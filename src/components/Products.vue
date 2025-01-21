63c440fbcac69c0a3fb771fb56470b4b68157fbc
PC <dijanic@gmail.com>
2025-01-21 10:40:16 +0100

    * fetch products from API

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,32 +1,27 @@
 <template>
 
-  <ul>
-    <li v-for="phone in phones" :key="phone.name">
-      {{ phone.name }} - Price: {{ phone.price }} - Stock: {{ phone.amount }}
+  <p v-if="!fetchedProducts.length">Loading products...</p>
+  <p v-if="apiError">{{ apiError }}</p>
+
+  <ul v-if="fetchedProducts.length">
+    <li v-for="product in fetchedProducts" :key="product.id">
+      {{ product.title }} - ${{ product.price }} - Stock: {{ product.stock }}
     </li>
   </ul>
   
 </template>
 
 <script>
+  import axios from "axios";
+
   export default {
 
     name: 'Products',
 
     data() {
       return {
-        phones: [
-          { name: 'iPhone 15', price: 1500, amount: 32 },
-          { name: 'Samsung Galaxy S23', price: 1200, amount: 0 },
-          { name: 'Google Pixel 8', price: 900, amount: 15 },
-          { name: 'OnePlus 11', price: 800, amount: 0 },
-          { name: 'Sony Xperia 5 IV', price: 1000, amount: 8 },
-          { name: 'Huawei P50 Pro', price: 1100, amount: 5 },
-          { name: 'Xiaomi Mi 13', price: 700, amount: 20 },
-          { name: 'LG Velvet', price: 650, amount: 0 },
-          { name: 'Motorola Edge 40', price: 750, amount: 12 },
-          { name: 'Nokia XR20', price: 600, amount: 7 }
-        ]
+        fetchedProducts: [],
+        apiError: null,
       }
     },
 
@@ -43,7 +38,9 @@
     },
 
     mounted() {
-      console.log('Mounted')
+      axios.get('https://dummyjson.com/products')
+          .then(response => this.fetchedProducts = response.data.products)
+          .catch(() => this.apiError = "Failed to load products. Please try again later.");
     }
   }
 </script>
